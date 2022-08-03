99dd206bc77b82f43740c5d8f25cb5234f250eed
54V15 <david.olulowo@gmai.com>
2022-08-03 10:26:23 -0600

    Update all

---
+++ guess.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int random = 4;
+    cout << "I'm thinking of a number! Try to guess it" << endl;
+    int guess;
+    cin >> guess;
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main(){
+    cout << "Hello, there! Whats's your name? ";
+    string name;
+    cin >> name;
+    if(name == "David"){
+        cout << "Hey! That's My Name!" << endl;
+    }else{
+        cout << "Hello, " << name << "!" << endl;
+    }
+    return 0;
+    cout << "Hello, " << name << "!" << endl; 
+}
